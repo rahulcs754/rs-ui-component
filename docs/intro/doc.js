311a279d67e606e48eb83d38ab942c0ec8c5053e
rahul shukla <rahul.shukla754@gmail.com>
2022-02-08 01:32:24 +0530

    added pages and csss

--- docs/intro/doc.js
+++ docs/intro/doc.js
@@ -9,3 +9,11 @@ $(".code").click(function() {
 	toastr.options.positionClass = "toast-top-right";
 	toastr.success('&nbsp; &nbsp; Copied to the clipboard');
 });
+
+
+const codeBlocks = document.querySelectorAll("pre code");
+    codeBlocks.forEach((codeBlock) => {
+        let code = codeBlock.innerHTML;
+        code = code.replaceAll("<", "&LT");
+        codeBlock.innerHTML = code;
+    });
